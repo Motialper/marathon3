eefcb4279475aab02ccf42d9d872f4df189333c2
Motialper <motialp@ubuntu.myguest.virtualbox.org>
2023-11-15 13:09:19 +0200

    color

--- src/Component/ImageDisplay.js
+++ src/Component/ImageDisplay.js
@@ -1,4 +1,7 @@
+
 import './ImageDisplay.css';
+import axios from 'axios';
+
 import { useRef, useState, useEffect } from "react";
 
 
@@ -7,6 +10,10 @@ const ImageDisplay = (props) => {
     const [Color, setColor] = useState('red')
     const [image_only_disp, setimage_only_disp] = useState('');
 
+
+
+
+    console.log('aaa', props.ShowImg)
     useEffect(() => {
         if(props.image_only === true) {
             setimage_only_disp('image_only_disp');
@@ -24,6 +31,27 @@ const ImageDisplay = (props) => {
     const ColorChange = (e) => {
         setColor(e.target.value)
     }
+
+    const func_choosh_color = (e) => {
+        if ( props.ShowImg && props.ShowImg !== 'no_bg:false' ){
+            console.log(e.target.value)
+
+            let formData = new FormData();
+
+            formData.append('UploadedFileName',  props.ShowImg)
+            formData.append('color',  Color)
+
+            axios.post('http://localhost:5000/upload_img_with_color', formData)
+                .then(res => {
+                    props.setShowImg(res.data)
+                    props.color_func()
+
+                })
+                
+
+        }
+      
+    }
     
     return (
         <div>
@@ -37,7 +65,7 @@ const ImageDisplay = (props) => {
                 <div className='color_' onClick={focusInput} >צבע רקע
                     <span className='color_choosh' style={{backgroundColor : Color}}></span>
                 </div>
-                <input type='color' ref={inputElement}  className='inputColor' onChange={ColorChange}/>
+                <input type='color' ref={inputElement}  className='inputColor' onChange={ColorChange} onBlur={func_choosh_color}/>
                 </>
                 : ''}
                 <div className={`image_pace ${image_only_disp}`}>
