a63166266de196d3b115798a8fb9aaa0b1bc5ee6
Motialper <motialp@ubuntu.myguest.virtualbox.org>
2023-11-15 13:09:19 +0200

    color

--- server/app.js
+++ server/app.js
@@ -10,10 +10,19 @@ app.use(cors())
 
 app.use(express.static('upload_img'))
 app.use(express.static('upload_img_no_bg'))
+app.use(express.static('upload_img_with_color'))
+const fs = require('fs');
+const axios = require('axios');
+const FormData = require('form-data');
+const path = require('path');
+
 
 
 
 app.post('/upload_img', function (req, res) {
+  if (!req.files || !req.files.myFile) {
+    return res.status(400).send('No file uploaded.');
+  }
   
   console.log(req.files)
 
@@ -29,10 +38,7 @@ app.post('/upload_img', function (req, res) {
       return res.status(500).send(err);
     } else {
 
-      const axios = require('axios');
-      const FormData = require('form-data');
-      const fs = require('fs');
-      const path = require('path');
+     
 
       const inputPath = `${__dirname}/upload_img/${fileName}`;
       const formData = new FormData();
@@ -59,12 +65,53 @@ app.post('/upload_img', function (req, res) {
         res.send(fileName);
       })
       .catch((error) => {
-          return console.error('Request failed:', error);
+        console.error('Request failed:', error);
+        res.status(500).send(error.message);
       });
           }
         });
 
         
       })
+      app.post('/upload_img_with_color', function (req, res) {
+  
+        console.log(req)
+      
+        
+        let fileName=req.body.UploadedFileName;
+        let color=req.body.color;
 
+
+      
+      
+            const inputPath = `${__dirname}/upload_img_no_bg/no_bg:${fileName}`;
+            const formData = new FormData();
+            formData.append('size', 'auto');
+            formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));
+            formData.append('bg_color', color)
+            axios({
+              method: 'post',
+              url: 'https://api.remove.bg/v1.0/removebg',
+              data: formData,
+              responseType: 'arraybuffer',
+              headers: {
+                ...formData.getHeaders(),
+                'X-Api-Key': 'qC1yTxAfNgkd6HnVCwVM1cGU',
+              },
+              encoding: null
+            })
+            .then((response) => {
+              if(response.status != 200) return console.error('Error:', response.status, response.statusText);
+            
+              (async () => {
+              fs.writeFileSync(`${__dirname}/upload_img_with_color/color:${fileName}`, response.data);
+            }) ();
+              res.send('color:'+fileName);
+            })
+            .catch((error) => {
+                return console.error('Request failed:', error);
+            });
+                }
+            );
+      
       app.listen(5000)
