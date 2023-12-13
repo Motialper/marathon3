f908fa82b454552c15e01c0a2a6af6cbdb79d513
Motialper <motialp@ubuntu.myguest.virtualbox.org>
2023-12-13 10:37:26 +0200

    server

--- server/app.js
+++ server/app.js
@@ -1,63 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const fileupload = require('express-fileupload');
 
-var cors = require('cors');
 app.use(cors());
-
-var fileupload = require("express-fileupload");
 app.use(fileupload());
-
 app.use(express.static('upload_img'));
 app.use(express.static('upload_img_no_bg'));
 app.use(express.static('upload_img_color'));
 
+const basePath = __dirname;
 
+const sendToAPI = require('./send_to_api');
 
-const sent_to_api =  require('./send_to_api');
+app.post('/upload_img', async function (req, res) {
+    try {
+        const time = new Date().getTime();
+        const fileName = `${time}_${req.files.UploadedFile.name}`;
+        const filePath = `${basePath}/upload_img/${fileName}`;
+        const fileNameDes = `${basePath}/upload_img_no_bg/no_bg_${fileName}`;
 
-app.post('/upload_img', function (req, res) {
+        req.files.UploadedFile.mv(filePath, err => {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                (async () => {
+                    await sendToAPI(filePath, '', fileNameDes);
+                    res.send(fileName);
+                })();
+            }
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send(error.message);
+    }
+});
 
-  
-    let time=new Date().getTime();
-    let fileName=time+"_"+req.files.UploadedFile.name;
+app.post('/upload_image_with_color', async function (req, res) {
+    try {
+        const fileName = req.body.UploadedFileName;
+        const color = req.body.color;
+        const inputPath = `${basePath}/upload_img_no_bg/${fileName}`;
+        const fileNameDes = `${basePath}/upload_img_color/color_${fileName}`;
 
-    req.files.UploadedFile.mv(`${__dirname}/upload_img/${fileName}`, err => {
+        (async () => {
+            await sendToAPI(inputPath, color, fileNameDes);
+            res.send(fileName);
+        })();
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send(error.message);
+    }
+});
 
-      if (err) {
-        res.status(500).send(err);
-      } else {
-          const inputPath = `${__dirname}/upload_img/${fileName}`;
-          const fileNameDes=`${__dirname}/upload_img_no_bg/no_bg_${fileName}`;
-
-          (async () => {
-              await sent_to_api(inputPath , '' , fileNameDes );
-              res.send(`${fileName}`);
-          }) ();
-
-     
-      }
-
-    });
-
- 
-})
-
-app.post('/upload_image_with_color', function (req, res) {
-
-  let fileName=req.body.UploadedFileName;
-  let color=req.body.color;
-
-  const inputPath = `${__dirname}/upload_img_no_bg/${fileName}`;
-
-  const fileNameDes=`${__dirname}/upload_img_color/color_${fileName}`;
-
-
-  (async () => {
-    await sent_to_api(inputPath , color , fileNameDes );
-    res.send(`${fileName}`);
-  }) ();
-
-
-})
-
-app.listen(5000);
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
--- server/send_to_api.js
+++ server/send_to_api.js
@@ -1,45 +1,40 @@
-
-
 const axios = require('axios');
 const FormData = require('form-data');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = async function send_to_api(fileName , color='' , fileNameDes ) {
-    
-  const inputPath = fileName; //`${__dirname}/upload_img_no_bg/${fileName}`;
-  const formData = new FormData();
-  formData.append('size', 'auto');
-  formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));
+module.exports = async function sendToAPI(fileName, color = '', fileNameDes) {
+    try {
+        const inputPath = fileName;
+        const formData = new FormData();
+        formData.append('size', 'auto');
+        formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));
 
-console.log('color:' + color);
+        if (color !== '') {
+            formData.append('bg_color', color);
+        }
 
-  if(color!='') {
-    formData.append('bg_color', color);
-  }
+        const response = await axios({
+            method: 'post',
+            url: 'https://api.remove.bg/v1.0/removebg',
+            data: formData,
+            responseType: 'arraybuffer',
+            headers: {
+                ...formData.getHeaders(),
+                'X-Api-Key': '7C8s8WBCSYzfSm2KBx44vsjE',
+            },
+            encoding: null
+        });
 
+        if (response.status !== 200) {
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
 
-  await axios({
-    method: 'post',
-    url: 'https://api.remove.bg/v1.0/removebg',
-    data: formData,
-    responseType: 'arraybuffer',
-    headers: {
-      ...formData.getHeaders(),
-      'X-Api-Key': '7C8s8WBCSYzfSm2KBx44vsjE',
-    },
-    encoding: null
-  })
-  .then((response) => {
-    if(response.status != 200) return console.error('Error:', response.status, response.statusText);
+        fs.writeFileSync(fileNameDes, response.data);
 
-    fs.writeFileSync( fileNameDes /*`${__dirname}/upload_img_color/color_${fileName}`*/, response.data);
-
-    return 'color_'+fileName;
-
-  })
-  .catch((error) => {
-      return console.error('Request failed:', error);
-  });
-
-}
+        return `color_${fileName}`;
+    } catch (error) {
+        console.error('Request failed:', error);
+        throw error; 
+    }
+};
